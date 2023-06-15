298172100eddafdc6883ae666b6aaaefc4839d77
AisyA72 <b72-2002-arceus@docomo.ne.jp>
2023-06-15 12:35:23 +0900

    微調整

--- trip-memory/src/App.js
+++ trip-memory/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages/Home"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Trip-Memory</h1>
       <Routes>
         <Route path="/" element={ <Home /> } />
+        <Route path="/login" element={ <Login /> } />
       </Routes>
     </div>
   );
